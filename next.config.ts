659e8e6ef355b6167030c48e38b7613b7d7fa22a
Salim Lagrichi <salimlagrichi2012@hotmail.com>
2025-07-15 18:01:50 +0100

    Add new feature

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // No static export settings for SSR/API support
+  images: {
+    domains: ['img.clerk.com'],
+  },
 };
 
 export default nextConfig;
