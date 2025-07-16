a7baa05d7c1995c65b91b0f1cd109ec0b744a93b
Salim Lagrichi <salimlagrichi2012@hotmail.com>
2025-07-16 11:01:47 +0100

    Restore ClerkProvider in _app.tsx only (no UsernameGate or Navbar) for isolation test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
 import '../app/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  console.log("Rendering _app with only Component");
-  return <Component {...pageProps} />;
+  console.log("Rendering _app with ClerkProvider only");
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 } 
